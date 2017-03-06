8e92b28b94661e8398107200c9714c398d2a87fc
Aman Bahata <bahataaman@gmail.com>
2017-03-06 20:48:11 +0000

    Adds routes to the API

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade' );
 
 app.use(require('./app_server/routes/index'));
+app.use(require('./app_api/routes/index'));
 
 var server = app.listen(app.get('port'), function(){
     console.log('Server listening on port ' + app.get('port'));
