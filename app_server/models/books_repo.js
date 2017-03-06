ba3bcef67ac0093ad26e4ced52613f42b874dd66
Aman Bahata <bahataaman@gmail.com>
2017-03-06 23:39:33 +0000

    Adds database schema to application

--- app_server/models/books_repo.js
+++ app_server/models/books_repo.js
@@ -2,11 +2,23 @@
  * Created by aman1 on 06/03/2017.
  */
 var mongoose = require('mongoose');
-var bookSchema = new mongoose.Schema({
-    
-        name: String,
-        author: String,
-        rating: Number
 
+var reviewSchema = new mongoose.Schema({
+    author: {type: String, required: true},
+    rating: {type: Number, required: true, min: 0, max: 3},
+    reviewText: String,
+    createdOn: {type: Date, 'default': Date.now}
 
 });
+
+var bookSchema = new mongoose.Schema({
+
+        name: {type: String, required: true},
+        author: {type: String, required: true},
+        rating: {type: Number, "default" : 0, min: 0, max: 3},
+        reviews: [reviewSchema]
+});
+
+
+
+mongoose.model('Book', bookSchema);
