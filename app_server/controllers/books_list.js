7d4e2410723de72055a3e7805f4bbc3b1aa5a4aa
Aman Bahata <bahataaman@gmail.com>
2017-03-06 17:53:42 +0000

    Moves specific information about a book detail to the controller

--- app_server/controllers/books_list.js
+++ app_server/controllers/books_list.js
@@ -7,14 +7,46 @@ module.exports.homeList = function (req, res) {
         title: 'Bookface',
         pageHeader: {
             title: 'Bookface',
-        }
+        },
+        books: [{
+            name: 'The Escape',
+            author: 'David Baldacci',
+            rating: 3
+        },{
+            name: 'The great gasby',
+            author: 'F. Scott Fitzgerald',
+            rating: 2
+        },{
+            name: 'The Grapes of Wrath',
+            author: 'John Steinbeck',
+            rating: 1
+        }]
 
     });
 }
 
 /*Get book info page*/
  module.exports.bookInfo = function (req, res) {
-     res.render('book_info', {title: 'Book Info'});
+     res.render('book_info', {
+         title: 'The escape',
+         pageHeader: {title: 'The escape'},
+         book: {
+             name: 'The escape',
+             author: 'David Baldacci',
+             rating: 3,
+             reviews: [{
+                 author: 'Aman Enghida',
+                 rating: 3,
+                 timestamp: '05/03/17',
+                 reviewText: 'I had spent a good time reading this book.'
+             },{
+                 author: 'Joh Doe',
+                 rating: 3,
+                 timestamp: '04/03/17',
+                 reviewText: 'I did not like this book.'
+             }]
+         }
+     });
  }
 
  /*Get add review page*/
