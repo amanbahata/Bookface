59c67b5d6ca4952b3c43bccd4c13d6faa67c43fe
Aman Bahata <bahataaman@gmail.com>
2017-03-23 13:11:52 +0000

    Adds user login authentication using passport module

---
+++ app_api/config/passport.js
@@ -0,0 +1,24 @@
+/**
+ * Created by aman1 on 23/03/2017.
+ */
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'},
+    function (username, password, done) {
+    User.findOne({email: username}, function (err, user) {
+        if (err){
+            return done(err);
+        }
+        if (!user){
+            return done(null, false, {
+                message : 'Incorrect username.'
+            });
+        }
+    });
+}));
