53ec776065c85a3c9056e668838d970e2f346306
Aman Bahata <bahataaman@gmail.com>
2017-03-23 13:22:33 +0000

    Adds registration functionality

--- app_api/config/passport.js
+++ app_api/config/passport.js
@@ -17,8 +17,35 @@ passport.use(new LocalStrategy({
         }
         if (!user){
             return done(null, false, {
-                message : 'Incorrect username.'
+                "message" : 'Incorrect username.'
             });
         }
     });
 }));
+
+
+module.exports.register = function (req, res) {
+    if (!req.body.name || !req.body.email || !req.body.password){
+        sendJSONresponse(res, 400, {
+           "message" : "All input field required."
+        });
+        return;
+    }
+
+    var user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.setPassword(req.body.password);
+
+    user.save(function(err){
+        var token;
+        if (err) {
+            sendJSONresponse(res, 404, err);
+        }else{
+            token = user.generateToken();
+            sendJSONresponse(res, 200, {
+                "token" : token
+            });
+        }
+    })
+};
