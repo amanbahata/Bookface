859ccd1dd99e33792547c8322d32503d809082c1
Aman Bahata <bahataaman@gmail.com>
2017-03-30 22:52:47 +0100

    Modifies the data book schema

--- app_api/models/books_repo.js
+++ app_api/models/books_repo.js
@@ -3,26 +3,46 @@
  */
 var mongoose = require('mongoose');
 
+// var reviewSchema = new mongoose.Schema({
+//     author: {type: String,required: true},
+//     rating: {type: Number, required: true, min: 0, max: 5},
+//     reviewText: {type: String, required: true},
+//     createdOn: {type: Date, 'default': Date.now}
+//
+// });
+//
+// var bookSchema = new mongoose.Schema({
+//
+//         name: {type: String, unique: true, required: true},
+//         description: {type: String, required: true},
+//         rating: {type: Number, 'default' : 0, min: 0, max: 5},
+//         reviews: [reviewSchema]
+// });
+//
+// var authorSchema = new mongoose.Schema({
+//     name: {type: String, unique: true,required: true},
+//     books: [bookSchema]
+// });
+
+
 var reviewSchema = new mongoose.Schema({
-    author: {type: String,required: true},
     rating: {type: Number, required: true, min: 0, max: 5},
+    screenName: {type:String, required:true},
     reviewText: {type: String, required: true},
     createdOn: {type: Date, 'default': Date.now}
 
 });
 
 var bookSchema = new mongoose.Schema({
-
-        name: {type: String, unique: true, required: true},
-        description: {type: String, required: true},
-        rating: {type: Number, 'default' : 0, min: 0, max: 5},
-        reviews: [reviewSchema]
-});
-
-var authorSchema = new mongoose.Schema({
-    name: {type: String, unique: true,required: true},
-    books: [bookSchema]
+    author: {type: String, require: true},
+    title: {type: String, required: true, unique: true},
+    description: {type: String, required: true },
+    reviews: [reviewSchema]
 });
 
 
-mongoose.model('Book', authorSchema);
+mongoose.model('Book', bookSchema);
+
+
+
+// mongoose.model('Book', authorSchema);
